7737e7fc21157152ee812f8ec9f912a4fd53e1e7
Alison Santos <alisonsantoso22@gmail.com>
2021-12-24 17:53:20 -0300

    Configurando o Particles.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ function App() {
     <div className="app-style">
       <Sidebar />
       <div className="main-content">
-
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+          <div className="line-4"></div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
